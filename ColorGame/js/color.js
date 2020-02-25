19f1e1c0d3c94913aa544d5c131b70e5771d8452
khalidkhan <khalid.khan@msn.com>
2020-02-25 13:44:09 +0000

    created functions randomColor n generateRandomColor

--- ColorGame/js/color.js
+++ ColorGame/js/color.js
@@ -1,4 +1,4 @@
-// creating an array of 12 rgb colors
+/* creating an array of 12 rgb colors
 var colors = [
   "rgb(255, 0, 0)",
   "rgb(255, 255, 255)",
@@ -13,6 +13,9 @@ var colors = [
   "rgb(45, 0, 255)",
   "rgb(0, 255, 255)"
 ];
+*/
+// generating 12 random color inn array
+var colors = generateRandomColor(12);
 
 // select colors from class .circle
 var circles = document.querySelectorAll(".circle");
@@ -67,4 +70,30 @@ function changeColor(color) {
 function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+// create function randomColor
+function randomColor() {
+    // pick "red" from 0-255
+    var r = Math.floor(Math.random() * 256);
+    // pick "green" from 0-255
+    var g = Math.floor(Math.random() * 256);
+    // pick "blue" from 0-255
+    var b = Math.floor(Math.random() * 256);
+    // returning rgb(r, g, b) color
+    return "rgb(" + r + ", " + g + ", " + b +")";
+}
+
+// creating function generateRandomColor, pass para "num"
+function generateRandomColor(num) {
+    // create an empty array
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        // adding randomColor to array bu calling randomColor()
+        arr.push(randomColor());
+    }
+
+    // return array
+    return arr;
 }
